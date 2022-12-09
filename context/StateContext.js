13e8aad4caa7fe0d7f670026aabd5a94ceb3a3da
Niraj Modi <108391974+modiniraj09@users.noreply.github.com>
2022-12-09 18:02:30 +0530

    Updated to context.js cart's bug
    Fixed Bug
    while incrementing and decrementing the no. of items in cart, the order of items changed
    used previous cart values and updated it.

--- context/StateContext.js
+++ context/StateContext.js
@@ -52,12 +52,18 @@ export const StateContext = ({ children }) => {
     const newCartItems = cartItems.filter((item) => item._id !== id)
 
     if(value === 'inc') {
-      setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 } ]);
+      setCartItems((prevCartItems) => {
+        prevCartItems[index] = {...foundProduct,quantity: foundProduct.quantity + 1,};
+        return prevCartItems;
+      });
       setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
       setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
     } else if(value === 'dec') {
       if (foundProduct.quantity > 1) {
-        setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 } ]);
+        setCartItems((prevCartItems) => {
+        prevCartItems[index] = {...foundProduct,quantity: foundProduct.quantity - 1,};
+        return prevCartItems;
+      });
         setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
         setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
       }
