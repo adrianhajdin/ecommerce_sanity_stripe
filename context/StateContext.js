e8113849898a48dd44133f9fd18079e86a065c65
Iyinoluwa <iyinoyelz69@gmail.com>
2023-01-03 00:14:28 -0700

    fixes cart items reordering

--- context/StateContext.js
+++ context/StateContext.js
@@ -49,15 +49,19 @@ export const StateContext = ({ children }) => {
   const toggleCartItemQuanitity = (id, value) => {
     foundProduct = cartItems.find((item) => item._id === id)
     index = cartItems.findIndex((product) => product._id === id);
-    const newCartItems = cartItems.filter((item) => item._id !== id)
-
+    
+    const newCartItems = [...cartItems];
     if(value === 'inc') {
-      setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 } ]);
+      foundProduct.quantity += 1;
+      newCartItems[index] = foundProduct;
+      setCartItems(newCartItems)
       setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
       setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
     } else if(value === 'dec') {
       if (foundProduct.quantity > 1) {
-        setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 } ]);
+        foundProduct.quantity -= 1;
+        newCartItems[index] = foundProduct
+        setCartItems(newCartItems)
         setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
         setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
       }
