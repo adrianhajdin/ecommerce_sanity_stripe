898ac5936f703a026237b7bc8ad9672214812f72
Jones <139799995+jones-sandbox@users.noreply.github.com>
2023-09-28 15:17:25 +0000

    Update Product.jsx

--- components/Product.jsx
+++ components/Product.jsx
@@ -15,7 +15,7 @@ const Product = ({ product: { image, name, slug, price } }) => {
             className="product-image"
           />
           <p className="product-name">{name}</p>
-          <p className="product-price">&#8373;{price}</p>
+          <p className="product-price">{price}</p>
         </div>
       </Link>
     </div>
