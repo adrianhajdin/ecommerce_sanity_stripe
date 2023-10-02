49bbc92fee5883ce555560e2a71484a80b1cc79b
Jones <139799995+jones-sandbox@users.noreply.github.com>
2023-10-02 14:05:17 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
 
 import { Layout } from '../components';
 import '../styles/globals.css';
